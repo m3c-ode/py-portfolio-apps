fe009a370ae4dbd73338048791287117ac617ca9
maxime m3.code <maxime.marechalmccoy@gmail.com>
2023-01-24 15:46:18 -0800

    layout with spaced columns

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import csv
 import pandas
 
-# st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 st.title("Portfolio Website")
 col1, col2 = st.columns(2)
@@ -26,35 +26,36 @@ with open("./data.csv") as file:
     data = list(csv.reader(file))
     df = pandas.read_csv("data.csv", sep=",")
 
-print(df)
-leftCol, rightCol = st.columns(2)
+# print(df)
+# passing a list with ratios in arguments, we can control the width of the columns
+leftCol, emptyCol, rightCol = st.columns([1.5, 0.5, 1.5])
 
-for i in range(1, len(data)):
-    [title, description, url, image] = data[i]
-    if i % 2 == 1:
-        with leftCol:
-            st.header(title)
-            st.image(f"images/{image}")
-            st.write(description)
-            st.write(url)
-    else:
-        with rightCol:
-            st.header(title)
-            st.image(f"images/{image}")
-            st.write(description)
-            st.write(url)
-
-# using pandas
-# for index, row in df.iterrows():
-#     if index < 10:
+# for i in range(1, len(data)):
+#     [title, description, url, image] = data[i]
+#     if i % 2 == 1:
 #         with leftCol:
-#             st.header(row["title"])
-#             st.image(f"images/{row['image']}")
-#             st.write(row["description"])
-#             st.write(row["url"])
+#             st.header(title)
+#             st.image(f"images/{image}")
+#             st.write(description)
+#             st.write(f"[Source Code]({row['url']})")
 #     else:
 #         with rightCol:
-#             st.header(row["title"])
-#             st.image(f"images/{row['image']}")
-#             st.write(row["description"])
-#             st.write(row["url"])
+#             st.header(title)
+#             st.image(f"images/{image}")
+#             st.write(description)
+#             st.write(url)
+
+# using pandas
+with leftCol:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.image(f"images/{row['image']}")
+        st.write(row["description"])
+        st.write(f"[Source Code]({row['url']})")
+
+with rightCol:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.image(f"images/{row['image']}")
+        st.write(row["description"])
+        st.write(row["url"])
